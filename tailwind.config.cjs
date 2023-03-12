8255698b988e71f65918a1470d751efc2f074b41
paisalll <faizaltriasaa@gmail.com>
2023-03-12 17:16:55 +0700

    Configurasi Daisyui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,28 @@ module.exports = {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        "primary": "#0b3c95",    
+        "secondary": "#84d2f3",                
+        "accent": "#fdd231",                
+        "neutral": "#e5e7eb",
+                
+        "base-100": "#1b44a6",
+                
+        "info": "#8DCAC1",
+                
+        "success": "#d9f99d",
+                
+        "warning": "#f59e0b",
+                
+        "error": "#dc2626",
+                },
+              },
+            ],
+          }, 
   theme: {
     extend: {},
   },
