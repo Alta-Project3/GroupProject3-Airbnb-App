be7f0581605b3bdf8114763a42be988e1a628b77
Mustafa Turner <mustafaturner@me.com>
2023-03-13 21:07:52 +0700

    added reservation date with disabled dates

--- src/Page/detailStay/index.tsx
+++ src/Page/detailStay/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AiFillStar } from 'react-icons/ai';
 import { useParams } from 'react-router';
 import Layout from '../../Components/Layout';
@@ -18,6 +18,51 @@ const DetailStay = () => {
   // Date Picker
   const [dateRange, setDateRange] = useState([null, null]);
   const [startDate, endDate] = dateRange;
+
+  // Disable Date Range
+  const [startDateDisable, setStartDateDisable] = useState<string>('2023-3-17')
+  const [endDateDisable, setEndDateDisable] = useState<string>('2023-3-20')
+  const getDatesInRange = (startDateStr: string, endDateStr: string): Date[] => {
+    const startDate = new Date(startDateStr);
+    const endDate = new Date(endDateStr);
+    const dates = [];
+
+    for (let currentDate = startDate; currentDate <= endDate; currentDate.setDate(currentDate.getDate() + 1)) {
+      dates.push(new Date(currentDate));
+    }
+
+    return dates;
+  };
+
+  const disabledDates: Date[] = getDatesInRange(startDateDisable, endDateDisable)
+
+  // Check if two arrays are unique
+  const uniqueArrays = (arr1: any[], arr2: any[]): boolean =>
+    !arr1.some(obj1 => arr2.some(obj2 => JSON.stringify(obj1) === JSON.stringify(obj2)));
+
+  // Change date format to string
+  const formatDate = (date: Date): string => {
+    return date.toLocaleDateString('en-US', { month: 'short', day: 'numeric' });
+  };
+
+  // Handle invalid date
+  const [dateWarning, setDateWarning] = useState(false);
+
+  const handleWarning = () => {
+    setDateWarning(true);
+  };
+
+  useEffect(() => {
+    if (dateWarning) {
+      const timeoutId = setTimeout(() => {
+        setDateWarning(false);
+      }, 2000);
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
+  }, [dateWarning]);
 
   return (
     <Layout>
@@ -52,17 +97,71 @@ const DetailStay = () => {
         </button>
         <DatePicker
           className="input input-primary bg-primary w-full"
+          placeholderText='Set reservation date'
           selectsRange={true}
           startDate={startDate}
           endDate={endDate}
           onChange={(update: any) => {
             setDateRange(update);
+
+            if (update[1] != null) {
+              // const chosenDates = update.map((date: Date) => date.toISOString().substring(0, 10));
+              const chosenDates = getDatesInRange(update[0], update[1])
+              console.log(chosenDates, disabledDates)
+              if (uniqueArrays(chosenDates, disabledDates)) {
+                console.log("true")
+                setDateRange(update);
+
+              } else {
+                handleWarning();
+                console.log("false");
+                setDateRange([null, null]);
+              }
+            }
+
           }}
+          dateFormat="d MMM yyyy"
+          minDate={new Date()}
+          excludeDates={disabledDates}
           isClearable={true}
+          clearButtonClassName="btn btn-ghost"
         />
-        <p>{JSON.stringify(dateRange)}</p>
+      </div>
+
+      <div className='flex justify-center static m-0 w-screen border-t-4 border-primary py-4'>
+        <div
+          className={`${dateWarning
+            ? 'opacity-100 transition-opacity duration-500'
+            : 'opacity-0 hidden transition-opacity duration-500'
+            } alert alert-warning shadow-lg w-10/12`}
+        >
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
+            <span>Invalid Date</span>
+          </div>
+        </div>
+
+        <div className={`${dateWarning
+          ? 'opacity-0 transition-opacity duration-500 hidden'
+          : 'opacity-100 transition-opacity duration-500'
+          } flex items-center justify-between w-10/12`}>
+          <div>
+            {dateRange[1] !== null ?
+              <p>{formatDate(dateRange[0] || new Date())} - {formatDate(dateRange[1] || new Date())}</p> :
+              <></>
+            }
+
+            <p>Rp. {stay?.price.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1.")}/night</p>
+          </div>
+          {dateRange[1] !== null ?
+            <button className='btn btn-accent'>Reserve</button> :
+            <button disabled className='btn btn-accent'>Reserve</button>
+          }
+
+        </div>
 
       </div>
+
 
 
 
